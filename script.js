b48a373817ca52857812be8783042bd9640223f4
pollonet <30765345+pollonet@users.noreply.github.com>
2019-04-26 00:05:18 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,4 +3,5 @@
     var api = '8dfe678b2c7b300c005e01bc2b5c98a008dd13bc';
     var shorten_exclude = ['google.com', 'mysite.com', 'domain.com']; 
 </script>
+
 <script src='https://rawcdn.githack.com/riandiramdani/safelinku.com/dfea660cf01dddb004f50c30a69b4d2e58157e34/script.js'></script>
