1590e3f2879f8b9cb3440b5b485b4fcb9320fe76
pollonet <30765345+pollonet@users.noreply.github.com>
2019-04-26 00:01:56 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+<script type="text/javascript">
     var go_url = 'https://suarankri.me/';
     var api = '8dfe678b2c7b300c005e01bc2b5c98a008dd13bc';
     var shorten_exclude = ['google.com', 'mysite.com', 'domain.com']; 
+</script>
